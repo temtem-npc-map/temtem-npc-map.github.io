a60bbfc6a614ca7d300cf0a2026b3a17573f3ecc
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-11-12 14:51:43 -0500

    Create omninesia.js

---
+++ omninesia.js
@@ -0,0 +1,108 @@
+var markers = [];
+var map;
+
+$(document).ready(function() {  
+  
+    map = L.map('map-omninesia', {
+        crs: L.CRS.Simple,
+        attributionControl: false,
+        minZoom: 1,
+        preferCanvas: true
+    });
+  
+  var bounds = [[0,0], [1000,1000]];
+  var image = L.imageOverlay('images/maps/omninesia.png', bounds).addTo(map);
+  map.fitBounds(bounds);
+  
+  L.control.mousePosition().addTo(map);
+
+  var TemMarker = L.Icon.extend({
+    options: {
+        shadowUrl: '',
+        iconSize:     [39, 51], // size of the icon
+        shadowSize:   [0, 0], // size of the shadow
+        iconAnchor:   [22, 51], // point of the icon which will correspond to marker's location
+        shadowAnchor: [0, 0],  // the same for the shadow
+        popupAnchor:  [19, 0], // point from which the popup should open relative to the iconAnchor
+        tooltipAnchor: [24, -30]
+    }
+  });
+  var npcGenericIcon = new TemMarker({iconUrl: 'images/icons/npc.png'});
+  var npcMovingIcon = new TemMarker({iconUrl: 'images/icons/npc-moving.png'});
+  var npcVendorIcon = new TemMarker({iconUrl: 'images/icons/npc-vendor.png'});
+  var npcClothingIcon = new TemMarker({iconUrl: 'images/icons/npc-clothing.png'});
+  var npcFurnitureIcon = new TemMarker({iconUrl: 'images/icons/npc-furniture.png'});
+  var npcCombatIcon = new TemMarker({iconUrl: 'images/icons/npc-combat.png'});
+  var npcMovingCombatIcon = new TemMarker({iconUrl: 'images/icons/npc-moving-combat.png'});
+  var npcProfessorKonstantinosIcon = new TemMarker({iconUrl: 'images/icons/professor-konstantinos.png'});
+  var npcAdiaTurayIcon = new TemMarker({iconUrl: 'images/icons/adia-turay.png'});  
+  
+  // Use the tool over at https://codepen.io/susanpallmann/full/eYzbOyJ to format entries easier!
+  
+  //var name = L.marker([00, 00], {title: name, icon: npcGenericIcon}).bindTooltip("name", {className: 'tooltip'}).addTo(map);
+  //name._icon.classList.add("testclass");
+  //markers.push(name);
+  
+  function listSearchResults(query) {
+    $('#results').empty();
+    var counter = 0; 
+    for (var i in markers){
+      if (counter <= 4) {
+        var markerID = markers[i].options.title;
+        var markerToolTip = markers[i].getTooltip();
+        var content = markerToolTip.getContent();
+        var source = markers[i].options.icon.options.iconUrl;
+        if (markerID.includes(query)){
+          counter += 1;
+          $('#results').append('<li><a href="" class="result-link" id="' + markerID + '"><img class="marker-small" src="' + source + '" alt="" /><p>' + content + '</p></a></li>');
+        }
+      }
+    }
+  }
+  
+  function refreshSearch(query) {
+    $('.leaflet-marker-icon').addClass('hidden-icon');
+    $('*[class*=' + query + ']').removeClass('hidden-icon');
+  }
+  
+  $("#search").bind('input', function(){
+    let value = $("#search").val();
+    value = value.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '').toLowerCase();
+    if (value === '') {
+      $('.leaflet-marker-icon').removeClass('hidden-icon');
+      $('#results').empty();
+    } else {
+      refreshSearch(value);
+      listSearchResults(value);
+    }
+  });
+  
+  $('input[type="checkbox"]').bind('input', function(){
+    $('.leaflet-marker-icon').addClass('hidden-icon');
+    if ($('input:checkbox:checked').length) {
+      $('input:checkbox:checked').each(function(index) {
+        let key = $(this).val();
+        console.log(key);
+        $('.' + key).removeClass('hidden-icon');
+      });
+    } else {
+      $('.leaflet-marker-icon').removeClass('hidden-icon');
+    }
+  });
+});
+
+$(document).on('click','.result-link',function(e){
+  function markerFunction(id){
+    for (var i in markers){
+      var markerID = markers[i].options.title;
+      if (markerID === id){
+        markers[i].openTooltip();
+        map.panTo(markers[i].getLatLng());
+      } else {
+        markers[i].closeTooltip();
+      }
+    }
+  }
+  markerFunction($(this).attr('id'));
+  e.preventDefault();
+}); 
