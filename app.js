199614f633a3ca1a84a9be20f06805128b8d0b42
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-11-11 23:03:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('#menu').click(function(e) {
     if ($('#menu').attr('aria-checked') === 'true') {
-      $('#tray').removeClass('script-display-none').slideToggle();
+      $('#tray').slideToggle();
       $('#menu').find('.material-icons').text('menu');
       $('#menu').attr('aria-checked', 'false');
     } else {
-      $('#tray').removeClass('script-display-none').slideToggle();
+      $('#tray').slideToggle();
       $('#menu').find('.material-icons').text('clear');
       $('#menu').attr('aria-checked', 'true');
     }
