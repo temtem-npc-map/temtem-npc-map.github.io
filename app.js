1296eeddd0357560861ffc15a8f91db98013fbb1
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-11-11 23:07:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
-  $('#menu').click(function(e) {
-    console.log('this ran');
-    if ($('#menu').attr('aria-checked') === 'true') {
-      $('#tray').slideToggle();
-      $('#menu').find('.material-icons').text('menu');
-      $('#menu').attr('aria-checked', 'false');
-    } else {
-      $('#tray').slideToggle();
-      $('#menu').find('.material-icons').text('clear');
-      $('#menu').attr('aria-checked', 'true');
-    }
-  });
+});
+
+$('#menu').click(function(e) {
+  console.log('this ran');
+  if ($('#menu').attr('aria-checked') === 'true') {
+    $('#tray').slideToggle();
+    $('#menu').find('.material-icons').text('menu');
+    $('#menu').attr('aria-checked', 'false');
+  } else {
+    $('#tray').slideToggle();
+    $('#menu').find('.material-icons').text('clear');
+    $('#menu').attr('aria-checked', 'true');
+  }
 });
